f14faf5c91bc952001d5918c3432133fc7616dec
Georgei <georgeisevchenko@gmail.com>
2023-06-24 02:03:10 +0300

    Studing react

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -5,6 +5,7 @@ const Main = () => {
   return (
     <>
       <div className="Main">
+        <div className="SolidPage_1"></div>
         <div className="Page_1">
           <div className="ImgPage_1">
             <img src="./src/component/Img/Page 1/IMG_6431 1.jpg" />
@@ -31,14 +32,31 @@ const Main = () => {
             </div>
           </div>
         </div>
+        <div className="SolidPage_1"></div>
+
         <div className="Page_2">
           <h1>NIKASON BRAND это</h1>
           <div className="ContentPage_2">
             <div className="UlPage_2">
               <ul>
-                <li>Производство авторской мебели из массива</li>
-                <li>Реставрация, редизайн, ремонт деревянной мебели</li>
-                <li>Производство мебели по индивидуальным размерам</li>
+                <li>
+                  Производство авторской
+                  <br />
+                  мебели из массива
+                  <div className="Solid"></div>
+                </li>
+                <li>
+                  Реставрация, редизайн, ремонт
+                  <br />
+                  деревянной мебели
+                  <div className="Solid"></div>
+                </li>
+                <li>
+                  Производство мебели по
+                  <br />
+                  индивидуальным размерам
+                  <div className="Solid"></div>
+                </li>
               </ul>
             </div>
             <div className="AssortmentPage_2">
@@ -48,37 +66,37 @@ const Main = () => {
                   <tr>
                     <td>
                       <img src="./src/component/Img/Page 2/door-2 1.jpg" />
-                      Двери
+                      <p>Двери</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/closet 1.jpg" />
-                      Шкафы
+                      <p>Шкафы</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/chair-2 1.jpg" />
-                      Столы и стулья
+                      <p>Столы и стулья</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/drawers 1.jpg" />
-                      Комоды
+                      <p>Комоды</p>
                     </td>
                   </tr>
                   <tr>
                     <td>
                       <img src="./src/component/Img/Page 2/drawer 1.jpg" />
-                      Тумбы
+                      <p>Тумбы</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/gate 1.jpg" />
-                      Арки
+                      <p>Арки</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/room-divider 1.jpg" />
-                      Перегородки
+                      <p>Перегородки</p>
                     </td>
                     <td>
                       <img src="./src/component/Img/Page 2/bed 1.jpg" />
-                      Кровати
+                      <p>Кровати</p>
                     </td>
                   </tr>
                 </tbody>
@@ -86,6 +104,7 @@ const Main = () => {
             </div>
           </div>
         </div>
+
         <div className="Page_3">
           <h1>NIKASON BRAND это семейная мастеркая</h1>
           <div className="ContentPage_3">
@@ -95,26 +114,67 @@ const Main = () => {
                 <h2>Саргылана и Никита Колодезниковы-Матчитовы</h2>
                 <p>
                   Саргылана - рестовратор-декоратор, преображает и дарит вторую
-                  жизнь старой мебели. 
-                  <br/>
-                  <br/>Никита - мастер по дереву, вся мебель
-                  делается его умелыми руками.
+                  жизнь старой мебели.
+                  <br />
+                  <br />
+                  Никита - мастер по дереву, вся мебель делается его умелыми
+                  руками.
                 </p>
                 <div className="ButtonPage_2">
                   <button className="More about us">Подробнее о нас</button>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="Page_3_AboutUs_2">
+          <h2>Посмотрите видео о нас</h2>
+          <div className="VideoAboutUs_2">
+            <video>
+              <source src="./src/component/Img/Page 3406081364.mp4"></source>
+            </video>
+          </div>
+        </div>
 
-            <div className="AboutUs_2">
-            <h2>Посмотрите видео о нас</h2>
-            <div className="VideoAboutUs_2">
-              
+        <div className="Page_4">
+          <h1 className="HeadingPage_4">Наши услуги</h1>
+          <div className="Contents1Page_4">
+            <div className="ImgPage_4">
+              <img src="./src/component/Img/Page 4/IMG_5907.jpg" />
             </div>
+            <div className="ContentPage_4">
+              <h1>Мебель на заказ</h1>
+              <h2>
+                Мебель под заказ по индивидуальным размерам это гарантия
+                отличного качества, долговечность, экологичность и
+                эксклюзивность наших изделий. Возможность создания своего
+                неповторимого стиля, уникальность и неповторимость каждого
+                изделия.
+              </h2>
+              <div className="ButtonPage_4">
+                <button>Смотреть услуги</button>
+              </div>
+            </div>
+          </div>
+          <div className="Contents2Page_4">
+            <div className="Img2Page_4">
+              <img src="./src/component/Img/Page 4/IMG_5645.jpg" />
+            </div>
+            <div className="Content2Page_4">
+              <h1>Мастер-классы</h1>
+              <h2>
+                Что делать, если память о предках хочется сохранить, но к вашему
+                интерьеру старая и испорченная мебель не подходит? Правильно,
+                отреставрировать его и наслаждаться не только обновлённым видом
+                мебели, но и с теплотой помнить, что эта мебель была сделана
+                талантливыми предками.
+              </h2>
+              <div className="Button2Page_4">
+                <button>Смотреть услуги</button>
+              </div>
             </div>
           </div>
         </div>
-        <div className="Page_4"></div>
         <div className="Page_5"></div>
       </div>
     </>
